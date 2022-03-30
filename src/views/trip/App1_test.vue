04e7e407cfca2bc2a102a3af0be797dff755d6e2
kimsj4814 <kimsj4814@naver.com>
2022-03-30 15:28:13 +0900

    commit

---
+++ src/views/trip/App1_test.vue
@@ -0,0 +1,135 @@
+<template>
+<div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Separated link</a>
+          </div>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-sm-2" type="text" placeholder="Search">
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</div>
+
+
+
+
+<div class="container">
+
+  <div class="search_box">
+
+  </div>
+
+   <div class="row">
+      <div class="col-3">
+          <p>Card</p>
+          <div class="card">
+            <div class="card-header">
+              My Card
+            </div>
+            <img src="./1234.jpg" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Lorem</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada.</p>
+              <a href="#" class="btn btn-primary">More</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-3">
+          <p>Card</p>
+          <div class="card">
+            <div class="card-header">
+              My Card
+            </div>
+            <img src="images/card-image.png" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Lorem</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada.</p>
+              <a href="#" class="btn btn-primary">More</a>
+            </div>
+          </div>
+        </div>
+
+         <div class="col-3">
+          <p>Card</p>
+          <div class="card">
+            <div class="card-header">
+              My Card
+            </div>
+            <img src="images/card-image.png" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Lorem</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada.</p>
+              <a href="#" class="btn btn-primary">More</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-3">
+          <p>Card</p>
+          <div class="card">
+            <div class="card-header">
+              My Card
+            </div>
+            <img src="images/card-image.png" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Lorem</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada.</p>
+              <a href="#" class="btn btn-primary">More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      
+
+</div>
+
+</template>
+
+<script>
+
+
+</script>
+
+<style scope>
+  .search_box{
+    background-color: blue;
+    height: 100px;
+  }
+</style>

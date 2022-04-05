5e1c1ae9fa70e4c2bfce62181e7d0970d49b01b6
yjjs2004 <98498650+yjjs2004@users.noreply.github.com>
2022-04-05 15:39:13 +0900

    List 커밋

---
+++ src/components/community/community_pageDo.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li v-if="currentpage !== 1" class="page-item">
+          <a style="cursor: pointer" class="page-link" @click="getList(currentpage - 1)">
+            Previous
+          </a>
+        </li>
+        <li
+          v-for="page in pagelist"
+          :key="page" 
+          class="page-item"
+          :class="currentpage == page ? 'active' : ''"
+        >
+          <a style="cursor: pointer" class="page-link" @click="getList(page)">{{page}}</a>
+        </li>
+        <li v-if="maxpage > currentpage" class="page-item">
+          <a style="cursor: pointer" class="page-link" @click="getList(currentpage + 1)">Next</a>
+        </li>
+      </ul>
+    </nav>
+</template>
+
+<script>
+import {useStore} from 'vuex';
+import {watch, ref} from 'vue';
+export default {
+  setup(){
+    const store = useStore();
+    const maxpage = ref(0);
+    const currentpage = ref(1);
+    const pagelist = ref([]);
+
+    
+    watch(()=>store.state.obj,
+        ()=>{
+          maxpage.value = store.state.obj.maxpage;
+          currentpage.value = store.state.obj.currentpage;
+          pagelist.value = store.state.obj.pagelist;
+        }
+    );
+
+        const getList = (page) =>{
+			store.dispatch('store_page', page);
+    }
+   
+    
+    return{
+      getList,currentpage,pagelist, maxpage
+    }
+	}
+
+}
+</script>
+
+<style scoped>
+     ul.pagination{justify-content: center;}
+</style>
---
+++ src/views/community/commu_list.vue
@@ -0,0 +1,43 @@
+<template>
+  <Board_List/>      
+  <Paging />
+  <button type="button" class="btn btn-info float-right" @click="goWrite">글 쓰 기</button>            
+</template>
+
+<script>
+import Board_List from '../../components/community/community_list.vue';
+import Paging from '../../components/community/community_pageDo.vue';
+
+import { useRouter} from 'vue-router'
+export default {
+	components: {
+		Board_List,Paging
+	},
+  props:{
+    parent_id:{
+      type:String,
+      required:false     
+    }
+  },
+	emits:['parent_getSession'],
+
+	setup(props,context){
+
+    context.emit("parent_getSession");
+   
+    const router = useRouter();
+    const goWrite=()=>{
+      router.push({
+        name:'Board_Write',
+      });
+    }
+
+    return {
+      goWrite
+    }
+  }
+
+}
+
+
+</script>

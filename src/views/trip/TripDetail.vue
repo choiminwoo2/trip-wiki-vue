5c37561475bb46ce2640d7e366aca430f57a29bf
kimsj4814@naver.com <user.email>
2022-04-05 14:25:59 +0900

    04/05 12:25  커밋

---
+++ src/views/trip/TripDetail.vue
@@ -0,0 +1,199 @@
+<template>
+<div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Separated link</a>
+          </div>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-sm-2" type="text" placeholder="Search">
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</div>
+
+
+
+
+<div class="container">
+  <div class="title">
+      <h1>망방해변</h1>
+      <hr class="line">
+  </div>
+  <div class="main_top">
+   
+    <div class="main_left">
+      <img src="./1234.jpg" alt="">
+    </div>  
+
+    <div class="main_right">
+      <img src="./map.png" alt="">
+    </div>
+
+  </div>
+
+  <div class="main_bot"> 
+
+    <div class="bot_top">
+
+      <div class="bot_left">
+        <p>지역 | 강원</p>
+        <p>주소 | 강원도 삼척시 근덕면 망방해변로</p>
+        <p>연락처 | 관광과 033-680-3356</p>
+      </div>
+
+      <div class="bot_right">
+        03/22(화)&nbsp;&nbsp;&nbsp; 03/23(수)&nbsp;&nbsp;&nbsp; 03/24(목)&nbsp;&nbsp;&nbsp; 03/25(금) <br><br>       
+        날씨정보 API
+      </div>
+     
+    </div>
+    
+    <div class="bot_bot">
+      <hr>
+      관광지 정보 <br><br>
+
+      지리산 자락을 수놓으며 굽이굽이 흘러가는 섬진강을 따라가면 매화나무가 자천으로 심어져있는 섬진마을이있다. 이 마을의 농가들은 산과
+      밭에 고식 대신 모든 매화나무를 심어 매년3월이 되면 하얗게 만개한 매화꽃이 마치 백설이 내린듯, 또는 하얀 꽃구름이 골짜기에 내려앉은듯 
+      장관을 이룬다. <br>
+      이곳의 맑고 온화한 강바람과 알맞게 피어오르는 물안개가 매실농사에 적합해서 수확량도 한 부락에서 연간 100톤이 넘는다.
+      올매화인 이곳의 매화는 지리산 능선에 잔설이 희끗희끗하게 남아 있는 3월 초순 경부터 꽃망울을 터트리며, 매실 수확은 지리산 철쭉이 한창
+      피어나는 6월에 시작된다.
+        지리산 자락을 수놓으며 굽이굽이 흘러가는 섬진강을 따라가면 매화나무가 자천으로 심어져있는 섬진마을이있다. 이 마을의 농가들은 산과
+      밭에 고식 대신 모든 매화나무를 심어 매년3월이 되면 하얗게 만개한 매화꽃이 마치 백설이 내린듯, 또는 하얀 꽃구름이 골짜기에 내려앉은듯 
+      장관을 이룬다. <br><br>
+      이곳의 맑고 온화한 강바람과 알맞게 피어오르는 물안개가 매실농사에 적합해서 수확량도 한 부락에서 연간 100톤이 넘는다.
+      올매화인 이곳의 매화는 지리산 능선에 잔설이 희끗희끗하게 남아 있는 3월 초순 경부터 꽃망울을 터트리며, 매실 수확은 지리산 철쭉이 한창
+      피어나는 6월에 시작된다.
+        지리산 자락을 수놓으며 굽이굽이 흘러가는 섬진강을 따라가면 매화나무가 자천으로 심어져있는 섬진마을이있다. 이 마을의 농가들은 산과
+      밭에 고식 대신 모든 매화나무를 심어 매년3월이 되면 하얗게 만개한 매화꽃이 마치 백설이 내린듯, 또는 하얀 꽃구름이 골짜기에 내려앉은듯 
+      장관을 이룬다. <br>
+      이곳의 맑고 온화한 강바람과 알맞게 피어오르는 물안개가 매실농사에 적합해서 수확량도 한 부락에서 연간 100톤이 넘는다.
+      올매화인 이곳의 매화는 지리산 능선에 잔설이 희끗희끗하게 남아 있는 3월 초순 경부터 꽃망울을 터트리며, 매실 수확은 지리산 철쭉이 한창
+      피어나는 6월에 시작된다.
+
+    </div>
+  </div>
+
+</div>
+
+
+</template>
+
+<script>
+
+
+</script>
+
+<style scope>
+
+.title{
+  margin-top: 100px;
+  text-align: center;
+  justify-content: center;
+}
+
+.line {
+  margin-left:  30%;
+  margin-right: 30%;
+}
+
+
+.main_top {
+  margin-top: 50px;
+ display: flex;
+ height: 400px;
+}
+
+.main_left{
+  margin-right: 10px;
+  width: 55%;
+  height: 100%;
+  background-color: blue;
+}
+
+.main_left > img{
+ width: 100%;
+ height: 100%;
+}
+
+.main_right {
+  width:45%;
+}
+
+.main_right > img {
+  width:100%;
+  height: 100%;
+  
+}
+
+.main_bot{
+  margin-top: 30px;
+  border-style: solid;
+  border-width: 1px;
+  width: 100%;
+  height: 650px;
+  
+}
+
+.bot_top{
+  display: flex;
+}
+
+.bot_left{
+  padding-top: 30px;
+  padding-left: 50px;
+  width: 100%;
+  height: 150px;
+}
+
+.bot_right{
+  padding-top: 30px;
+  text-align: center;
+  width: 100%;
+  height: 150px;
+}
+
+.bot_bot{
+  padding-left: 50px;
+  padding-right: 50px;
+  width: 100%;
+  height: 600px;
+}
+
+
+
+</style>
---
+++ src/views/trip/TripList.vue
@@ -0,0 +1,251 @@
+<template>
+<div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Separated link</a>
+          </div>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-sm-2" type="text" placeholder="Search">
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</div>
+
+<div class="container">
+
+  <div class="search_box">
+
+  <div class="btn-group mt-3" role="group" aria-label="Button group with nested dropdown">
+    
+      <button type="button" class="btn btn-primary" style="width:100px">지역</button>
+      <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
+      <div class="dropdown-menu" aria-labelledby="btnGroupDrop1" style="">
+        <a class="dropdown-item" href="#">Dropdown link</a>
+        <a class="dropdown-item" href="#">Dropdown link</a>
+      </div>
+      <input type="text" class="form-control" placeholder="지역 검색" aria-label="Recipient's username" aria-describedby="button-addon2" style="margin-left:10px;width:800px;" >
+      <button class="btn btn-primary" type="button" id="button-addon2" style="width:100px">검색</button>
+</div>
+
+
+  </div>
+
+  <div class="main">
+  <div class="row">
+      <div class="col-3">
+        
+          <div class="card">
+            <div class="card-header">
+              My Card
+            </div>
+            <img src="./1234.jpg" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Lorem</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada.</p>
+              <a href="#" class="btn btn-primary">More</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-3">
+        
+          <div class="card">
+            <div class="card-header">
+              My Card
+            </div>
+            <img src="./1234.jpg" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Lorem</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada.</p>
+              <a href="#" class="btn btn-primary">More</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-3">
+        
+          <div class="card">
+            <div class="card-header">
+              My Card
+            </div>
+            <img src="./1234.jpg" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Lorem</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada.</p>
+              <a href="#" class="btn btn-primary">More</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-3">
+        
+          <div class="card">
+            <div class="card-header">
+              My Card
+            </div>
+            <img src="./1234.jpg" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Lorem</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada.</p>
+              <a href="#" class="btn btn-primary">More</a>
+            </div>
+          </div>
+        </div>
+   </div>
+
+   <div class="row">
+      <div class="col-3">
+        
+          <div class="card">
+            <div class="card-header">
+              My Card
+            </div>
+            <img src="./1234.jpg" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Lorem</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada.</p>
+              <a href="#" class="btn btn-primary">More</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-3">
+        
+          <div class="card">
+            <div class="card-header">
+              My Card
+            </div>
+            <img src="./1234.jpg" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Lorem</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada.</p>
+              <a href="#" class="btn btn-primary">More</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-3">
+        
+          <div class="card">
+            <div class="card-header">
+              My Card
+            </div>
+            <img src="./1234.jpg" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Lorem</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada.</p>
+              <a href="#" class="btn btn-primary">More</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-3">
+        
+          <div class="card">
+            <div class="card-header">
+              My Card
+            </div>
+            <img src="./1234.jpg" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">Lorem</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada.</p>
+              <a href="#" class="btn btn-primary">More</a>
+            </div>
+          </div>
+        </div>
+   </div>
+  </div>
+
+
+
+
+<div>
+  <ul class="pagination">
+    <li class="page-item disabled">
+      <a class="page-link" href="#">&laquo;</a>
+    </li>
+    <li class="page-item active">
+      <a class="page-link" href="#">1</a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="#">2</a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="#">3</a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="#">4</a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="#">5</a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="#">&raquo;</a>
+    </li>
+  </ul>
+</div>
+      
+</div>
+
+</template>
+
+<script>
+
+
+</script>
+
+<style scope>
+.search_box{
+   margin-top: 50px;
+  transform: translateX(130px);
+ 
+} 
+
+  .row {
+    margin-top: 70px;
+  
+  }
+
+  .row>*{
+    z-index: -1;
+  }
+
+  .pagination{
+    margin-top: 100px;
+    justify-content: center;
+  }
+</style>

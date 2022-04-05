0c3633a3eedec2fc1cdc0eb37dc2233a3b285b60
yjjs2004 <98498650+yjjs2004@users.noreply.github.com>
2022-04-05 15:49:02 +0900

    List

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/member/LoginView.vue';
 import JoinView from '@/views/member/JoinView.vue';
 import MainPage from '@/views/main/MainPage.vue';
+import CommunityView  from '@/views/community/community_list.vue';
 const routes = [
   {
     path: '/',
@@ -17,6 +18,11 @@ const routes = [
     path: '/trip/login',
     name: 'Login',
     component: LoginView
+  },
+  {
+    path: '/trip/community',
+    name: 'Community',
+    component : CommunityView
   }
 ]
 

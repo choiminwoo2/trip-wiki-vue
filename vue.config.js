24c8c1e797980e7ea44604cbacf43571ccbf8b02
wjrdjs005 <wjrdjs005@naver.com>
2022-04-05 14:10:00 +0900

    join and login service add(22/04/05)

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir : "C:/eduStudy/SecurityToBoot/demo/src/main/resources/static",
+  //index.html 경로
+  indexPath : "C:/eduStudy/SecurityToBoot/demo/src/main/resources/templates/index.html",
+
+  publicPath:"/vue/",
 })

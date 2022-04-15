68a58456e2ce999321c842b1c1ce2324295181df
wjrdjs005 <wjrdjs005@naver.com>
2022-04-15 15:15:23 +0900

    commitMessage

---
+++ src/components/member/kakaoJoin.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="join-container">
+      <h1>처음 로그인하신 고객님은 회원가입에 동의하셔야 합니다.</h1>
+      <button class="btn btn-info" @click="kakaoJoin">회원 가입에 동의하기</button>
+  </div>
+</template>
+
+<script>
+import axios from '@/setting/axiossetting.js';
+import { useRoute } from 'vue-router'
+import router from '@/router';
+
+export default {
+    setup(){
+        const id = useRoute().params.id;
+        const callAxiosJoin = async() => {
+            console.log(id); 
+            const data= await (await axios.post(`kokaoJoins/${id}`)).data;
+            if(data ==1){
+                router.push({
+                    name: 'Home'
+                })
+            }
+        }
+
+        const kakaoJoin = () =>{
+            callAxiosJoin();
+        }
+        return{
+            kakaoJoin
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .join-container{
+        text-align: center;
+    }
+
+
+</style>
---
+++ src/views/member/kakaoJoin.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="join-container">
+    <router-link :to="{name:'Home'}"><img src="@/assets/logo.png"></router-link>
+    <KakaoJoin/>
+  </div>
+</template>
+
+<script>
+import KakaoJoin from '@/components/member/kakaoJoin.vue';
+export default {
+    components :{
+        KakaoJoin
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@media screen and (min-width:1024px){
+  .join-container{
+    display: flex;
+    flex-direction: column;
+    min-height: 100%;
+    margin : 0 auto;
+    max-width: 1100px;
+    justify-items: center;
+    padding-top:50px;
+  }
+  img{
+    position: relative;
+    max-width:200px;
+    max-height: 200px;
+    margin: 0 auto;
+    transform: translateX(3px);
+  }
+  a{
+    text-align: center;
+  }
+}
+/* 태블릿 pc~ */
+/* 가로의 길이가 768에서 1023px 사이 */
+@media screen and (min-width: 768px) and (max-width:1023px) {
+   .join-container{
+    display: flex;
+    flex-direction: column;
+    min-height: 100%;
+    margin : 0 auto;
+    max-width: 70%;
+    justify-items: center;
+    transform: translateX(-10px);
+    padding-top:50px;
+  } 
+   img{
+    position: relative;
+    max-width:200px;
+    max-height: 200px;
+    margin: 0 auto;
+  }
+  a{
+    text-align: center;
+  }
+}
+/* 모바일~*/
+/* 가로의 길이가  767 이하*/
+@media screen and (max-width:767px){
+   .join-container{
+    padding-top:50px;
+    display: flex;
+    flex-direction: column;
+    min-height: 100%;
+    margin : 0 auto;
+    justify-items: center;
+    min-width: 300px;
+    max-width: 600px;
+  }
+   img{
+    position: relative;
+    max-width:125px;
+    max-height: 125px;
+    margin: 0 auto;
+  }
+  a{
+    text-align: center;
+  }
+}
+</style>

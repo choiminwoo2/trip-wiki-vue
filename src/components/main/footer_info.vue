7b144f98421f2ec6cdf638b1b057b5a6b2429eb1
kimsj4814@naver.com <user.email>
2022-04-18 11:46:01 +0900

    푸터 수정 api 출처 달기

--- src/components/main/footer_info.vue
+++ src/components/main/footer_info.vue
@@ -8,10 +8,22 @@
             <span>(최민우, 이진수, 지태영, 김선재, 박세은)</span>
         </div>
         <div class="copyright">
+            
             <a href="https://www.data.go.kr/tcs/dss/selectApiDataDetailView.do?publicDataPk=15057787">
                 <img src="@/assets/img_opentype.png" alt="공공데이터포털 바로가기">
             </a>
+           
+            <a href="https://api.visitkorea.or.kr/main.do">
+            <img src="@/assets/tour_api.png" alt="한국관광공사 바로가기">
+            </a>
+            <div class="img3">  
+                <a href="https://data.kma.go.kr/cmmn/main.do">
+                <img src="@/assets/weather_api.png" alt="기상자료개방포털 바로가기">
+                </a>
+             </div>
+           
         </div>
+          
     </footer>
 </template>
 
@@ -49,6 +61,24 @@ export default {
 
     .copyright {
         text-align: center;
+        display: flex;
+        
     }
+
+    .copyright > a > img {
+        margin-top: 50px;
+        margin-right: 1.5rem;
+        
+    }
+
+   
+
+    .copyright > .img3 {
+        margin-left: -8%;
+        margin-top: 20px;
+    }
+
+
+    
     
 </style>
